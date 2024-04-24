5cda4689ea3ab39fb60f329909c02e79e681b130
flauts <hector.nieto@utec.edu.pe>
2024-04-24 09:04:59 -0500

    inicio

---
+++ src/main/java/com/proyecto/utec_roomie/UtecRoomieApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.utec_roomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtecRoomieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtecRoomieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/utec_roomie/UtecRoomieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.utec_roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtecRoomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
